689d1b6d7b4883e89e12164321423e3a3cecf1e0
golfpmac <pmcpherson@hotmail.com>
2019-07-11 13:42:37 -0700

    Added code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions 
 
-## Write a short comment describing this function
+## this function creates function type 'makeCacheMatrix' and saves the matrix
+## with lexical scoping
 
+browser()
 makeCacheMatrix <- function(x = matrix()) {
-
+     m <- NULL
+     set <- function(y) {
+          x <<- y
+          m <<- NULL
+     }
+     
+     get <- function() x
+     setsolve <- function(solve) m <<- solve
+     getsolve <- function() m
+     list(set = set, get = get,
+          setsolve = setsolve,
+          getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## this function checks to see if the the matrix 'x' is already cached
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+     m <- x$getsolve()
+     if(!is.null(m)) {
+          message("getting cached data")
+          return(m)
+     }
+     
+     data <- x$get()
+     m <- solve(data, ...)
+     x$setsolve(m)
+     m
 }
